1cca00d352a0eff4e954a9fd98c1f60f1917641b
Kiera Reidy <keelouise@yahoo.com>
2025-06-19 22:09:43 +0100

    Adding reset password option. KR 19/06/2025

--- skillstack/users/urls.py
+++ skillstack/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import (
     register_view, email_login_view, logout_view,
     verify_2fa_code, resend_code,
@@ -10,4 +11,8 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('verify/', verify_2fa_code, name='verify_code'),
     path('resend/', resend_code, name='resend_code'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
