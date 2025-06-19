ca4795430babcc2456daefe3a760b2b9a669d762
Kiera Reidy <keelouise@yahoo.com>
2025-06-19 22:03:37 +0100

    Setting up 2FA - in progress. KR 19/06/2025

--- skillstack/users/urls.py
+++ skillstack/users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import register_view, CustomLoginView, logout_view
+from .views import (
+    register_view, email_login_view, logout_view,
+    verify_2fa_code, resend_code,
+)
 
 urlpatterns = [
     path('register/', register_view, name='register'),
-]
-
-urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', email_login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('verify/', verify_2fa_code, name='verify_code'),
+    path('resend/', resend_code, name='resend_code'),
 ]
