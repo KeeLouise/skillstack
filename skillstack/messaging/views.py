22572121e5a90dd8b4273e2eea9c6862ac1eed88
Kiera Reidy <keelouise@yahoo.com>
2025-06-27 14:10:49 +0100

    Updated views for message function. KR 27/06/2025

--- skillstack/messaging/views.py
+++ skillstack/messaging/views.py
@@ -8,4 +8,28 @@ def inbox(request):
     messages = Message.objects.filter(recipient=request.user)
     return render(request, 'messaging/inbox.html', {'messages': messages})
 
+@login_required
+def sent_messages(request):
+    messages = Message.objects.filter(sender=request.user)
+    return render (request, 'messaging/sent.html', {'messages': messages})
 
+@login_required
+def message_detail(request, pk):
+    message=get_object_or_404(Message, pk=pk, recipient=request.user)
+    message.is_read = True
+    message.save()
+    return render(request, 'messaging/message_detail.html', {'message': message})
+
+@login_required
+def compose_message(request):
+    if request.method =='POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            msg = form.save(commit=False)
+            msg.sender = request.user
+            msg.save()
+            return redirect ('inbox')
+        
+        else:
+            form = MessageForm()
+        return render(request, 'messaging/compose.html', {'form: form'})
