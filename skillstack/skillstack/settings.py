c439526f39e4e5b8cce25d74765a03bbf1a78ec9
Kiera Reidy <keelouise@yahoo.com>
2025-07-24 12:58:19 +0100

    Updated allowed hosts. KR 24/07/2025

--- skillstack/skillstack/settings.py
+++ skillstack/skillstack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-dev-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'skillstack-1bx8.onrender.com']
 
 
 # Application definition
