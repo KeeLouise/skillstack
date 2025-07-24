c5a07144fa7aecef37cce80acbf946301c733263
Kiera Reidy <keelouise@yahoo.com>
2025-07-24 12:55:36 +0100

    Removed unnecessary middleware line. KR 24/07/2025

--- skillstack/skillstack/settings.py
+++ skillstack/skillstack/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
