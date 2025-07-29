eec8b06b3067ee8054aa9a4d24c56f144dc3c7ef
Kiera Reidy <keelouise@yahoo.com>
2025-07-29 20:35:18 +0100

    Updated to remove django otp. KR 29/07/2025

--- skillstack/skillstack/settings.py
+++ skillstack/skillstack/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_otp.middleware.OTPMiddleware',
 ]
 
 ROOT_URLCONF = 'skillstack.urls'
