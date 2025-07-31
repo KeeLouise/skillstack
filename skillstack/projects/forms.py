039defbd4b483f8d334b78f55e3ef2477596ce81
Kiera Reidy <keelouise@yahoo.com>
2025-07-31 09:51:16 +0100

    Updated Project Form to have a date selector. KR 31/07/2025

--- skillstack/projects/forms.py
+++ skillstack/projects/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Project
 
 class ProjectForm(forms.ModelForm):
+    start_date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        required=False
+    )
+    end_date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        required=False
+    )
+
     class Meta:
         model = Project
-        fields = ['title', 'description', 'start_date', 'end_date', 'status', 'category',]
+        fields = ['title', 'description', 'start_date', 'end_date', 'status', 'category']
