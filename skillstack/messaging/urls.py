2f5d58141a05b568e370f87f898d5f2df4fe0bac
Kiera Reidy <keelouise@yahoo.com>
2025-08-08 09:19:22 +0100

    Updated URLs. KR 08/08/2025

--- skillstack/messaging/urls.py
+++ skillstack/messaging/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_messages, name='messages'),  # main page shows all messages
-    path('inbox/', views.inbox, name='inbox'),      # only received messages
-    path('sent/', views.sent_messages, name='sent'),
-    path('compose/', views.compose_message, name='compose'),
+    path('', views.inbox, name='messages'),                  # Inbox
+    path('all/', views.all_messages, name='all_messages'),   # All Messages
+    path('sent/', views.sent_messages, name='sent'),         # Sent
+    path('compose/', views.compose_message, name='compose'), # Compose
     path('<int:pk>/', views.message_detail, name='message_detail'),
     path('<int:pk>/reply/', views.reply_message, name='reply_message'),
     path('<int:pk>/delete/', views.delete_message, name='delete_message'),
