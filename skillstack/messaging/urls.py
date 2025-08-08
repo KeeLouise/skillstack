7fadd7a411d4d519dae04682e87016c2b8f37c85
Kiera Reidy <keelouise@yahoo.com>
2025-08-08 08:47:53 +0100

    URL added for message replies. KR 08/08/2025

--- skillstack/messaging/urls.py
+++ skillstack/messaging/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_messages, name='messages'),
+    path('', views.inbox, name='messages'),
+    path('sent/', views.sent_messages, name='sent'),
     path('compose/', views.compose_message, name='compose'),
-    path('sent/', views.sent_messages, name='sent'), 
     path('<int:pk>/', views.message_detail, name='message_detail'),
     path('<int:pk>/reply/', views.reply_message, name='reply_message'),
     path('<int:pk>/delete/', views.delete_message, name='delete_message'),
