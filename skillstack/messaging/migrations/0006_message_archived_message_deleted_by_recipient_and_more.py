2e57a943410a3b2898dc1f8580187173cb49a52a
Kiera Reidy <keelouise@yahoo.com>
2025-08-11 19:05:57 +0100

    Migration fix for render. KR 11/08/2025

---
+++ skillstack/messaging/migrations/0006_message_archived_message_deleted_by_recipient_and_more.py
@@ -0,0 +1,8 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('messaging', '0005_backfill_attachment_size'),
+    ]
+
+    operations = []
