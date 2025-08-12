a91973b098cdaf6d846e608bf0d73cfaffbe2a45
Kiera Reidy <keelouise@yahoo.com>
2025-08-12 13:25:01 +0100

    Further updates. KR 12/08/2025

--- skillstack/projects/urls.py
+++ skillstack/projects/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     create_project, project_detail, edit_project, delete_project,
-    upload_project_attachments, download_project_attachment, delete_project_attachment
+    upload_project_attachments, download_project_attachment, delete_project_attachment,
 )
 
 urlpatterns = [
@@ -10,7 +10,8 @@ urlpatterns = [
     path('<int:pk>/edit/', edit_project, name='edit_project'),
     path('<int:pk>/delete/', delete_project, name='delete_project'),
 
-    path('<int:pk>/attachments/upload/', upload_project_attachments, name='project_attachments_upload'),
-    path('attachments/<int:att_pk>/download/', download_project_attachment, name='project_attachment_download'),
-    path('attachments/<int:att_pk>/delete/', delete_project_attachment, name='project_attachment_delete'),
+    # Attachments
+    path('<int:pk>/attachments/upload/', upload_project_attachments, name='upload_project_attachments'),
+    path('attachments/<int:att_pk>/download/', download_project_attachment, name='download_project_attachment'),
+    path('attachments/<int:att_pk>/delete/', delete_project_attachment, name='delete_project_attachment'),
 ]
