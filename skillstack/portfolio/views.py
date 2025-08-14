efbde23e95659f9d1dc52cbd1e7e7114fd0b0a72
Kiera Reidy <keelouise@yahoo.com>
2025-08-14 17:19:11 +0100

    Added messages import. KR 14/08/2025

--- skillstack/portfolio/views.py
+++ skillstack/portfolio/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import PortfolioLink
