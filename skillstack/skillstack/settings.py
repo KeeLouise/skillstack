68c17b14119a0f6dfbc2fcbf19ba43df4020f7b6
Kiera Reidy <keelouise@yahoo.com>
2025-08-15 22:03:25 +0100

    Missing comma added. KR 15/08/2025

--- skillstack/skillstack/settings.py
+++ skillstack/skillstack/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_cryptography'
+    'django_cryptography',
 
     # Third-party apps
     'crispy_forms',
