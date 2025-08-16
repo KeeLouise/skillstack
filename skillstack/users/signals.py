a018757383f1105046b5d9461ba001247881f1c9
Kiera Reidy <keelouise@yahoo.com>
2025-08-16 20:40:38 +0100

    Fixing admin issue. KR 16/08/2025

--- skillstack/users/signals.py
+++ skillstack/users/signals.py
@@ -3,6 +3,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver                     
 from .models import Profile
 
+User = get_user_model()
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
@@ -10,3 +12,11 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     else:
         # Ensure related Profile data gets saved whenever the User saves - KR 15/08/2025
         instance.profile.save()
+
+@receiver(post_save, sender=User)
+def ensure_profile(sender, instance, created, **kwargs):
+    """
+    Make sure every User has a Profile.
+    Works for newly created users and for existing users saved again.
+    """
+    Profile.objects.get_or_create(user=instance)
