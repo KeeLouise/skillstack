91018140ffee52e92c70568f813a2a5b49381e5a
Kiera Reidy <keelouise@yahoo.com>
2025-08-16 13:29:06 +0100

    Updates. KR 16/08/2025

--- skillstack/skillstack/settings.py
+++ skillstack/skillstack/settings.py
@@ -125,13 +125,18 @@ AUTHENTICATION_BACKENDS = [
     'users.backends.EmailBackend',             
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')       
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '') 
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'No-Reply-SkillStack <email.skillstack@gmail.com>')
+EMAIL_TIMEOUT = 30 
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER or "email.skillstack@gmail.com")
+SERVER_EMAIL = os.environ.get("SERVER_EMAIL", DEFAULT_FROM_EMAIL)
+EMAIL_SUBJECT_PREFIX = "[SkillStack] "
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
