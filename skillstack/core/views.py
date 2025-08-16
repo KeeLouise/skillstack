7fb8ca299aceca53e82f553d63e5342e131ca669
Kiera Reidy <keelouise@yahoo.com>
2025-08-16 19:34:18 +0100

    Fix tests. KR 16/08/2025

--- skillstack/core/views.py
+++ skillstack/core/views.py
@@ -4,24 +4,6 @@ from django.db.models import Q
 from projects.models import Project
 from projects.forms import ProjectForm
 
-def home(request):
-    if request.user.is_authenticated:
-        return redirect('dashboard')
-    return render(request, 'home.html')
-
-def error_400(request, exception=None):
-    return render(request, "errors/400.html", status=400)
-
-def error_403(request, exception=None):
-    
-    return render(request, "errors/403.html", status=403)
-
-def error_404(request, exception=None):
-    return render(request, "errors/404.html", status=404)
-
-def error_500(request):
-    return render(request, "errors/500.html", status=500)
-
 @login_required
 def dashboard(request):
     user = request.user
