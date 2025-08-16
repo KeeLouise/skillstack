1ee3cde14c6475428bbc85b27683bc89b03da885
Kiera Reidy <keelouise@yahoo.com>
2025-08-16 20:50:45 +0100

    Fixed. kR 16/08/2025

--- skillstack/users/signals.py
+++ skillstack/users/signals.py
@@ -1,22 +1,15 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save         
-from django.dispatch import receiver                     
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from .models import Profile
 
 User = get_user_model()
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    else:
-        # Ensure related Profile data gets saved whenever the User saves - KR 15/08/2025
-        instance.profile.save()
-
-@receiver(post_save, sender=User)
 def ensure_profile(sender, instance, created, **kwargs):
     """
-    Make sure every User has a Profile.
-    Works for newly created users and for existing users saved again.
+    Ensure every User has a Profile.
+    Safe for both newly-created and existing users.
     """
     Profile.objects.get_or_create(user=instance)
